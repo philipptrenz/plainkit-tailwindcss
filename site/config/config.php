f38d888a36c48901aec7f1c49465e6892b19eb48
philipptrenz <mail@philipptrenz.de>
2021-09-15 10:04:13 +0200

    enable languages by default, add sitemap

---
+++ site/config/config.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'languages' => true,
+    'languages.detect' => true,
+    'debug' => false,
+    'routes' => [
+        [
+          'pattern' => 'sitemap.xml',
+          'action'  => function() {
+              $pages = site()->pages()->index();
+    
+              // fetch the pages to ignore from the config settings,
+              // if nothing is set, we ignore the error page
+              $ignore = kirby()->option('sitemap.ignore', ['error']);
+    
+              $content = snippet('sitemap', compact('pages', 'ignore'), true);
+    
+              // return response with correct header type
+              return new Kirby\Cms\Response($content, 'application/xml');
+          }
+        ],
+        [
+          'pattern' => 'sitemap',
+          'action'  => function() {
+            return go('sitemap.xml', 301);
+          }
+        ]
+    ]
+];
