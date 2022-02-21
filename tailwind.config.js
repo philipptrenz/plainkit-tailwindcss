f13ec794fcaf93395412b9f2a138bcc1bbee6a72
philipptrenz <mail@philipptrenz.de>
2022-02-21 11:06:57 +0100

    upgrade to tailwindcss v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,8 @@ module.exports = {
     extend: { },
   },
   plugins: [ ],
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-        './site/**/*.php',
-        './site/tailwind/*.css',
-    ],
-  },
+  content: [
+    './site/**/*.php',
+    './site/tailwind/*.css',
+  ],
 }
