a1d1f28285f5157c15a339c1200cef23ab229089
philipptrenz <mail@philipptrenz.de>
2022-04-04 17:45:54 +0200

    remove redundant tags

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -3,14 +3,6 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
-  <meta name="robots" content="index,follow" />
-  <title><?= $page->title() ?></title>
-  <?php /*
-  <link rel="manifest" href="/assets/favicon/site.webmanifest">
-  <link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg" color="#4d6173">
-  <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="theme-color" content="#4d6173">
-  */ ?>
   <?= snippet('seo/meta') ?>
   <?= snippet('seo/favicon', [
     'themeColor' => '',
