9283efaa9910dac7448221c5877d5b195aa74f64
philipptrenz <mail@philipptrenz.de>
2022-04-04 16:00:43 +0200

    add robots.txt plugin

--- site/config/config.php
+++ site/config/config.php
@@ -3,28 +3,55 @@
 return [
     'languages' => true,
     'languages.detect' => true,
-    'debug' => true,
+    'debug' => false,
     'routes' => [
-        [
-          'pattern' => 'sitemap.xml',
-          'action'  => function() {
-              $pages = site()->pages()->index();
-    
-              // fetch the pages to ignore from the config settings,
-              // if nothing is set, we ignore the error page
-              $ignore = kirby()->option('sitemap.ignore', ['error']);
-    
-              $content = snippet('sitemap', compact('pages', 'ignore'), true);
-    
-              // return response with correct header type
-              return new Kirby\Cms\Response($content, 'application/xml');
-          }
-        ],
-        [
-          'pattern' => 'sitemap',
-          'action'  => function() {
-            return go('sitemap.xml', 301);
-          }
-        ]
-    ]
+      [
+      'pattern' => 'sitemap.xml',
+      'action'  => function() {
+          $pages = site()->pages()->index();
+
+          // fetch the pages to ignore from the config settings,
+          // if nothing is set, we ignore the error page
+          $ignore = kirby()->option('sitemap.ignore', ['error']);
+
+          $content = snippet('sitemap', compact('pages', 'ignore'), true);
+
+          // return response with correct header type
+          return new Kirby\Cms\Response($content, 'application/xml');
+        }
+      ],
+      [
+        'pattern' => 'sitemap',
+        'action'  => function() {
+          return go('sitemap.xml', 301);
+        }
+      ]
+    ],
+    'bnomei.robots-txt' => [
+      'sitemap' => 'sitemap.xml',
+      'groups' => function() {
+        if (site()->globalNoIndex()->toBool()) {
+          return [
+            '*' => [
+              'disallow' => [
+                '/',
+              ]
+            ]
+          ];
+        } else {
+          return [
+            '*' => [ // user-agent
+              'disallow' => [
+                  '/kirby/',
+                  '/site/',
+                  '/storage/',
+              ],
+              'allow' => [
+                  '/media/',
+              ]
+            ]
+          ];
+        }
+      },
+    ],
 ];
